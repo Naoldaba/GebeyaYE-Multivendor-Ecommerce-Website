658cffa8cf4078c98a7ac2ce34acc81604c03057
SosiSis <sosinasisay30@gmail.com>
2024-01-31 01:32:39 +0300

    unit test

--- src/components/Nav.test.jsx
+++ src/components/Nav.test.jsx
@@ -1,22 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { fireEvent } from '@testing-library/dom';
 import Nav from './Nav';
+import { AuthProvider } from './AuthContext';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('Nav', () => {
   test('renders the logo correctly', () => {
-    render(<Nav />, { wrapper: MemoryRouter });
-    
+    render(
+      <Router>
+        <AuthProvider>
+          <Nav />
+        </AuthProvider>
+      </Router>
+    );
+
     const logoText = screen.getByText('Gebeyaዬ');
     const logoSubText = screen.getByText('ሱቅ ከእልፍኜ');
 
     expect(logoText).toBeInTheDocument();
     expect(logoSubText).toBeInTheDocument();
   });
-
-
-})
-
- 
+});
---
+++ src/pages/ProductCataloge.test.jsx
@@ -0,0 +1,84 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import ProductCatalog from './ProductCataloge';
+import { AuthProvider } from '../components/AuthContext';
+
+// Mock the addToCart and cart functions
+const addToCart = jest.fn();
+const cart = [];
+
+// Mock the products array
+const products = [
+  { _id: 1, name: 'Product 1', category: 'Electronics', price: 10, imageurl: 'image1.jpg', description: 'Description 1' },
+  { _id: 2, name: 'Product 2', category: 'Clothes', price: 20, imageurl: 'image2.jpg', description: 'Description 2' },
+];
+
+test('renders products correctly', () => {
+  render(
+    <AuthProvider>
+      <ProductCatalog
+        products={products}
+        addToCart={addToCart}
+        cart={cart}
+      />
+    </AuthProvider>
+  );
+
+  // Assert that the products are rendered correctly
+  const renderedProducts = screen.getAllByRole('img');
+  expect(renderedProducts.length).toBe(products.length);
+
+  // Assert that the product information is displayed correctly
+  const productNames = screen.getAllByText(/product \d/i);
+  expect(productNames.length).toBe(products.length);
+
+  const productPrices = screen.getAllByText(/price: \d+Br\./i);
+  expect(productPrices.length).toBe(products.length);
+});
+
+test('addToCart should be called when "Add to Cart" button is clicked', () => {
+  const addToCart = jest.fn(); // Create a new mock function
+
+  render(
+    <AuthProvider>
+      <ProductCatalog
+        products={products}
+        addToCart={addToCart}
+        cart={cart}
+      />
+    </AuthProvider>
+  );
+
+  const addToCartButtons = getAllByText('Add to Cart');
+
+  fireEvent.click(addToCartButtons[0]);
+
+  expect(addToCart).toHaveBeenCalled();
+  expect(addToCart).toHaveBeenCalledTimes(1);
+});
+
+test('handleSocialShare should open the correct social sharing platform', () => {
+  render(
+    <AuthProvider>
+    <ProductCatalog
+      products={products}
+      addToCart={addToCart}
+      cart={cart}
+    />
+  </AuthProvider>
+  );
+
+  const facebookButtons = screen.getAllByRole('button', { name: /facebook/i });
+  const twitterButton = screen.getAllByRole('button', { name: /twitter/i });
+  const instagramButton = screen.getAllByRole('button', { name: /instagram/i });
+
+  window.open = jest.fn();
+
+  fireEvent.click(facebookButtons[0]);
+  expect(window.open).toHaveBeenCalledWith(expect.stringContaining('facebook.com'));
+
+  fireEvent.click(twitterButton[0]);
+  expect(window.open).toHaveBeenCalledWith(expect.stringContaining('twitter.com'));
+
+  fireEvent.click(instagramButton[0]);
+  expect(window.open).toHaveBeenCalledWith(expect.stringContaining('instagram.com'));
+});
